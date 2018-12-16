ab7c394251d17e858ca8898840146223dd182725
MrYangKD <Yyk930610@163.com>
2018-12-16 11:05:37 +0800

    提交文件

---
+++ src/main/java/com/yto/eurekademo/EurekademoApplication.java
@@ -0,0 +1,14 @@
+package com.yto.eurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekademoApplication.class, args);
+	}
+
+}
+
